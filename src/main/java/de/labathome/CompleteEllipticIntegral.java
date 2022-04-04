e1cb0983ee0308015e5ca61081bb535d99356267
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2022-04-04 20:53:06 +0200

    link code to algorithm in Bulirsch III article

--- src/main/java/de/labathome/CompleteEllipticIntegral.java
+++ src/main/java/de/labathome/CompleteEllipticIntegral.java
@@ -43,7 +43,8 @@ public class CompleteEllipticIntegral {
 		}
 
 		double m = 1.0; // \mu
-		double e = k_c;
+		double e = k_c; // \nu * \mu
+		// In the iterations, \nu_i is stored in k_c.
 
 		double f, q, g;
 
@@ -57,31 +58,33 @@ public class CompleteEllipticIntegral {
 			g = 1.0 - p;
 			f -= p;             // kc^2 - p
 			q *= b-a*p;         // (1 - kc^2)*(b-a*p)
-			p = Math.sqrt(f/g); // (kc^2 - p)/(1-p)                    --> p done here
-			a = (a-b)/g;        // (a-b)/(1-p)                         --> a done here
-			b = -q/(g*g*p)+a*p; // -(1 - kc^2)*(b-a*p)/( (1-p)^2 * p ) --> b done here
+			p = Math.sqrt(f/g); // sqrt((kc^2 - p)/(1-p))              --> p0 done here
+			a = (a-b)/g;        // (a-b)/(1-p)                         --> a0 done here
+			b = -q/(g*g*p)+a*p; // -(1 - kc^2)*(b-a*p)/( (1-p)^2 * p ) --> b0 done here
 		}
 
 		// iteration until convergence
-		boolean iterate = true;
-		while (iterate) {
-			f = a;
-			a += b/p;
-			g = e/p;
-			b += f*g;
-			b += b;
-			p += g;
-			g = m;
-			m += k_c;
-			if (Math.abs(g - k_c) > g*SQRT_EPS) {
-				k_c = Math.sqrt(e);
-				k_c += k_c;
-				e = k_c*m;
+		while (true) {
+			f = a;    // f = a_i
+			a += b/p; // a_{i+1} <-- a_i + b_i/p_i
+			g = e/p;  // g = (\nu_i*\mu_i) / p_i
+			b += f*g; // b_i + a_i * (\nu_i*\mu_i) /p_i
+			b += b;   // b_{i+1} <-- 2*( b_i + a_i * (\nu_i*\mu_i) /p_i )
+			p += g;   // p_{i+1} <-- p_i + (\nu_i*\mu_i) / p_i
+			g = m;    // g = mu_i
+			m += k_c; // mu_{i+1} <-- mu_i + \nu_i
+			if (Math.abs(g - k_c) > g*SQRT_EPS) { // |\mu_i - \nu_i| > \mu_i*EPS == |1 - \nu_i / \mu_i| > EPS, but more robust!
+				// not converged yet...
+				k_c = Math.sqrt(e); // k_c = sqrt(\nu_i * \mu_i)
+				k_c += k_c;         // \nu_{i+1} <-- 2*sqrt(\nu_i * \mu_i)
+				e = k_c*m; // (\nu * \mu)_{i+1} <-- \nu_{i+1} * mu_{i+1} (also update product explicitly)
 			} else {
-				iterate = false;
+				break;
 			}
 		}
 
+		// final approximation:
+		// \pi/2 * (a * \mu + b)/(\mu * (\mu + p))
 		return PI_2*(a*m+b)/(m*(m+p));
 	}
 
