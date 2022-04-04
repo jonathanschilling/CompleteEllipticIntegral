3eed41e279dbf9c647ba14c739f570b521b2c9d1
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2022-04-04 21:10:21 +0200

    single point demo

--- src/test/java/de/labathome/DemoEllipticIntegrals.java
+++ src/test/java/de/labathome/DemoEllipticIntegrals.java
@@ -6,6 +6,24 @@ public class DemoEllipticIntegrals {
 
 	public static void main(String[] args) {
 
+		run();
+
+		//demoEandK();
+	}
+
+	public static void run() {
+
+		// see where things to wrong for k_c --> 1
+		double kc = 1.0 - 1.0e-15;
+
+		double c1 = CompleteEllipticIntegral.cel(kc, 1, -1, 1);
+		double c2 = CompleteEllipticIntegral.cel(kc, kc*kc, -1, 1);
+
+		System.out.printf("cel(k_c,     1, -1, 1) = %g\n", c1);
+		System.out.printf("cel(k_c, k_c^2, -1, 1) = %g\n", c2);
+	}
+
+	public static void demoEandK() {
 		int n = 101;
 		double[] x = new double[n];
 
